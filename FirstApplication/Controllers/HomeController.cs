02217b6e564888a214e7607f17e288d56c1c6623
buibup <mongkolm@hatari.net>
2019-02-25 18:28:06 +0700

    completed ASP.NET Core for Beginners - Part 2

---
+++ FirstApplication/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(int id)
+        {
+            var upBook = new Book
+            {
+                Title = "Book is great",
+                Author = "buibup"
+            };
+
+            return View(upBook);
+        }
+    }
+}
---
+++ FirstApplication/Controllers/MusicController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstApplication.Controllers
+{
+    public class MusicController : Controller
+    {
+        public IActionResult Index()
+        {
+            var list = new List<Book>
+            {
+                new Book { Title = "Better today", Author = "best"}
+            };
+
+            return View(list);
+        }
+    }
+}
